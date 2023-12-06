0f0899bf0ca325541a842af2d1012274db8c46d3
Caspian Fang <xfang21@jhu.edu>
2023-12-06 07:43:18 +0000

    support sgmv_cutlass kernel, suffering from accuracy drops due to the intermediate output being stored in half-precision

--- csrc/punica/punica_ops.cc
+++ csrc/punica/punica_ops.cc
@@ -5,6 +5,8 @@
 #include <cstdint>
 
 #include "bgmv/bgmv_config.h"
+#include "sgmv/sgmv.h"
+#include "sgmv_flashinfer/sgmv_config.h"
 
 namespace {
 
@@ -40,6 +42,33 @@ inline constexpr uint32_t pack_u16(uint16_t a, uint16_t b) {
 
 #define CHECK_EQ(a, b)                                                         \
   TORCH_CHECK(a == b, "CHECK_EQ(" #a ", " #b ") failed. ", a, " vs ", b)
+
+#define CHECK_GE(a, b)                                                         \
+  TORCH_CHECK((a) >= (b), "CHECK_GE(" #a ", " #b ") failed. ", a, " vs ", b)
+
+//====== dispatch pytorch dtype ======
+
+#define _DISPATCH_SWITCH(cond, ...) \
+  [&]() -> bool {                   \
+    switch (cond) {                 \
+      __VA_ARGS__                   \
+      default:                      \
+        return false;               \
+    }                               \
+  }()
+
+#define _DISPATCH_DTYPE_CASE(enum_type, c_type_, ...) \
+  case enum_type: {                                   \
+    using c_type = c_type_;                           \
+    return __VA_ARGS__();                             \
+  }
+
+#define _DISPATCH_DTYPE_CASES(...)                                 \
+  _DISPATCH_DTYPE_CASE(at::ScalarType::Half, nv_half, __VA_ARGS__) \
+  _DISPATCH_DTYPE_CASE(at::ScalarType::BFloat16, nv_bfloat16, __VA_ARGS__)
+
+#define DISPATCH_TORCH_DTYPE(scalar_type, ...) \
+  _DISPATCH_SWITCH(scalar_type, _DISPATCH_DTYPE_CASES(__VA_ARGS__))
 
 //====== bgmv ======
 
@@ -550,6 +579,76 @@ void dispatch_bgmv_low_level(torch::Tensor y, torch::Tensor x, torch::Tensor w,
               " dtype=", x.scalar_type(), " out_dtype=", y.scalar_type());
 }
 
+//====== sgmv ======
+
+void dispatch_sgmv_cutlass(torch::Tensor y, torch::Tensor x,
+                           torch::Tensor w_ptr, torch::Tensor s,
+                           torch::Tensor tmp, int layer_idx) {
+  CHECK_INPUT(y);
+  CHECK_INPUT(x);
+  CHECK_INPUT(w_ptr);
+  CHECK_INPUT(s);
+  CHECK_INPUT(tmp);
+
+  CHECK_DIM(2, y);
+  CHECK_DIM(2, x);
+  CHECK_DIM(1, w_ptr);
+  CHECK_DIM(1, s);
+  CHECK_DIM(1, tmp);
+
+  int num_problems = s.size(0) - 1;
+  int d_in = x.size(1);
+  int d_out = y.size(1);
+  CHECK_EQ(tmp.size(0), static_cast<int64_t>(sgmv_tmp_size(num_problems)));
+  bool ok = DISPATCH_TORCH_DTYPE(x.scalar_type(), [&] {
+    return sgmv<c_type>((c_type*)y.data_ptr(), (c_type*)x.data_ptr(),
+                        (c_type**)w_ptr.data_ptr(), s.data_ptr<int32_t>(),
+                        tmp.data_ptr<uint8_t>(), num_problems, d_in, d_out,
+                        layer_idx);
+  });
+  TORCH_CHECK(ok, "No suitable kernel.", " dtype=", x.scalar_type());
+}
+
+void dispatch_sgmv_shrink(torch::Tensor y, torch::Tensor x, torch::Tensor w_ptr,
+                          torch::Tensor s, torch::Tensor tmp, int layer_idx) {
+  CHECK_INPUT(y);
+  CHECK_INPUT(x);
+  CHECK_INPUT(w_ptr);
+  CHECK_INPUT(s);
+  CHECK_INPUT(tmp);
+
+  CHECK_DIM(2, y);
+  CHECK_DIM(2, x);
+  CHECK_DIM(1, w_ptr);
+  CHECK_DIM(1, s);
+  CHECK_DIM(1, tmp);
+
+  uint32_t num_problems = s.size(0) - 1;
+  uint32_t d_in = x.size(1);
+  uint32_t d_out = y.size(1);
+  CHECK_EQ(tmp.scalar_type(), at::ScalarType::Byte);
+  CHECK_EQ(tmp.size(0), 8 * 1024 * 1024);
+
+#define CASE(_T, D_OUT)                                    \
+  case D_OUT:                                              \
+    return sgmv_shrink<c_type, D_OUT>(                     \
+        (c_type*)y.data_ptr(), (c_type*)x.data_ptr(),      \
+        (c_type**)w_ptr.data_ptr(), s.data_ptr<int32_t>(), \
+        tmp.data_ptr<uint8_t>(), num_problems, d_in, layer_idx);
+
+  bool ok = DISPATCH_TORCH_DTYPE(x.scalar_type(), [&] {
+    switch (d_out) {
+      FOR_SGMV_NARROW(CASE, c_type);
+      default:
+        return false;
+    }
+  });
+
+#undef CASE
+  TORCH_CHECK(ok, "No suitable kernel.", " dtype=", x.scalar_type(),
+              " d_out=", d_out);
+}
+
 } // namespace
 
 //====== pybind ======
@@ -560,4 +659,8 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("dispatch_bgmv", &dispatch_bgmv, "dispatch_bgmv");
   m.def("dispatch_bgmv_low_level", &dispatch_bgmv_low_level,
         "dispatch_bgmv_low_level");
+
+  m.def("sgmv_cutlass", &dispatch_sgmv_cutlass, "");
+  m.def("sgmv_cutlass_tmp_size", &sgmv_tmp_size, "");
+  m.def("sgmv_shrink", &dispatch_sgmv_shrink, "");
 }
--- vllm/lora/punica.py
+++ vllm/lora/punica.py
@@ -83,6 +83,11 @@ if import_exc is None:
         punica_kernels.dispatch_bgmv(y, buffer, wb_t_all, indicies, layer_idx,
                                      scale)
 
+        # punica_kernels.sgmv_cutlass(buffer, x, wa_t_all, indicies, layer_idx,
+        #                             1.0)
+        # punica_kernels.sgmv_cutlass(y, buffer, wb_t_all, indicies, layer_idx,
+        #                             scale)
+
     def add_lora_slice(y: torch.Tensor,
                        x: torch.Tensor,
                        wa_t_all: torch.Tensor,
@@ -149,6 +154,62 @@ if import_exc is None:
             y_slice_size,
             y_offset,
         )
+
+
+    def sgmv_cutlass(
+        y: torch.Tensor,
+        x: torch.Tensor,
+        w_ptr: torch.Tensor,
+        s: torch.IntTensor,
+        layer_idx: int,
+    ):
+        """
+      Semantics:
+        y[s[i]:s[i+1]] += x[s[i]:s[i+1]] @ deref(w_ptr[i])
+
+      Args:
+        y: Shape: `[B, H2]`. Output vectors. Will be changed in-place.
+        x: Shape: `[B, H1]`. Input vectors.
+        w_ptr: Shape: `[S]`. DType: torch.int64. Pointer to the weight matrices.\
+          Weight matrix shape: `[num_layers, H1, H2]`.
+        s: Shape: `[S+1]`, DType: torch.int32. Indptr of the weight matrices.\
+          `s[0] == 0`, `s[-1] == B`.
+        layer_idx: Layer index of the weight matrices.
+      """
+        tmp_size = punica_kernels.sgmv_cutlass_tmp_size(w_ptr.size(0))
+        tmp = torch.empty((tmp_size,), dtype=torch.uint8, device=x.device)
+        punica_kernels.sgmv_cutlass(y, x, w_ptr, s, tmp, layer_idx)
+
+
+    def add_lora_sgmv_cutlass(
+        y: torch.Tensor,
+        x: torch.Tensor,
+        wa_ptr: torch.Tensor,
+        wb_ptr: torch.Tensor,
+        s: torch.IntTensor,
+        layer_idx: int,
+        lora_rank: int,
+    ):
+        """
+      Semantics:
+        y[s[i]:s[i+1]] += x[s[i]:s[i+1]] @ deref(wa_ptr[i]) @ deref(wb_ptr[i])
+
+      Args:
+        y: Shape: `[B, H2]`. Output vectors. Will be changed in-place.
+        x: Shape: `[B, H1]`. Input vectors.
+        wa_ptr: Shape: `[S]`. DType: torch.int64. Pointer to the weight matrices.\
+          Weight matrix shape: `[num_layers, H1, R]`.
+        wb_ptr: Shape: `[S]`. DType: torch.int64. Pointer to the weight matrices.\
+          Weight matrix shape: `[num_layers, R, H2]`.
+        s: Shape: `[S+1]`, DType: torch.int32. Indptr of the weight matrices.\
+          `s[0] == 0`, `s[-1] == B`.
+        layer_idx: Layer index of the weight matrices.
+      """
+        tmp_size = punica_kernels.sgmv_cutlass_tmp_size(wa_ptr.size(0))
+        tmp = torch.empty((tmp_size,), dtype=torch.uint8, device=x.device)
+        v = torch.zeros((x.size(0), lora_rank), dtype=x.dtype, device=x.device)
+        punica_kernels.sgmv_cutlass(v, x, wa_ptr, s, tmp, layer_idx)
+        punica_kernels.sgmv_cutlass(y, v, wb_ptr, s, tmp, layer_idx)
 
 else:
 
