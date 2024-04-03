ba0de8c6000e9a2e2fbd7d412c01ed52fcde114c
SamuraiBUPT <31409163@bupt.edu.cn>
2024-04-03 03:41:06 +0000

    upload server.py

--- examples/api_server/server.py
+++ examples/api_server/server.py
@@ -14,6 +14,10 @@ from vllm.lora.request import LoRARequest,OLoRARequest
 
 import random
 import time
+import asyncio
+import trace
+from io import StringIO
+import cProfile
 
 TIMEOUT_KEEP_ALIVE = 5  # seconds.
 app = FastAPI()
@@ -54,7 +58,7 @@ async def generate(request: Request) -> Response:
     lora = request_dict.pop("lora", None)
     lora_request = None
     
-    estimated_tokens = request_dict['max_tokens']
+    estimated_tokens = request_dict.get("max_tokens", 50)
     
     if olora is not None:
         olora_request = OLoRARequest(LoRA_name_list[:-1],
@@ -75,16 +79,11 @@ async def generate(request: Request) -> Response:
                                             request_id,
                                             olora_request=olora_request,
                                             prefix_pos=prefix_pos)
-    elif lora_request is not None:
-        results_generator = engine.generate(prompt,
-                                            sampling_params,
-                                            request_id,
-                                            lora_request=lora_request,
-                                            prefix_pos=prefix_pos)
     else:
         results_generator = engine.generate(prompt,
                                             sampling_params,
                                             request_id,
+                                            lora_request=lora_request,
                                             prefix_pos=prefix_pos)
 
     # Streaming case
@@ -123,14 +122,86 @@ async def generate(request: Request) -> Response:
     return JSONResponse(ret)
 
 
+async def trace_request(engine, enable_olora=False, request_id="1"):
+    # we will make two requests
+    prompt = "To be or not to be, "
+    sam_para = SamplingParams(max_tokens=50)
+    
+    # print("========== LoRA =========")
+    # lora_request = all_lora_reqs[random.randint(0,1)]
+    # results_generator = engine.generate(prompt,
+    #                                     sam_para,
+    #                                     "1",
+    #                                     lora_request=lora_request)
+    
+    # final_output = None
+    # async for request_output in results_generator:
+    #     final_output = request_output
+
+    # assert final_output is not None
+    # prompt = final_output.prompt
+    # text_outputs = [prompt + output.text for output in final_output.outputs]
+    # ret = {"text": text_outputs}
+    # print(ret)
+    
+    if enable_olora:
+        print("========== OLoRA =========")
+        olora_request = OLoRARequest(LoRA_name_list[:-1],
+                                    olora_int_ids=LoRA_id_list[:-1],
+                                    lora_local_path=LoRA_Path_list[:-1],
+                                    self_id = 2, 
+                                    lora_reqs= all_lora_reqs[:-1])
+        
+        results_generator = engine.generate(prompt,
+                                            sam_para,
+                                            str(request_id),
+                                            olora_request=olora_request)
+        final_output = None
+        async for request_output in results_generator:
+            final_output = request_output
+            
+        assert final_output is not None
+        prompt = final_output.prompt
+        text_outputs = [prompt + output.text for output in final_output.outputs]
+        ret = {"text": text_outputs}
+        print(ret)
+        
+def run_trace(tracer, enable_olora=False):
+    tracer.run(f'asyncio.run(trace_request(engine, enable_olora={enable_olora}))')
+    
+def profile_program():
+    cProfile.run('asyncio.run(trace_request(engine, enable_olora=False))', 'profile_result_disable_olora.prof')
+    
+async def nsight_profile_program(engine):
+    import nvtx
+    with nvtx.annotate("TraceRequest", color="blue"):
+        await trace_request(engine, enable_olora=True, request_id="1")
+        
+    with nvtx.annotate("TraceRequest2", color="red"):
+        await trace_request(engine, enable_olora=True, request_id="2")
+
+# 定义一个自定义的文件类来捕获输出
+class CapturingFile(object):
+    def __init__(self):
+        self.content = []
+    def write(self, data):
+        self.content.append(data)
+    def flush(self):
+        pass
+    def getvalue(self):
+        return ''.join(self.content)
+
+
 if __name__ == '__main__':
     import os
+    import sys
     os.environ["CUDA_VISIBLE_DEVICES"] = "1"
     
+    enable_olora = True
     engine_args = AsyncEngineArgs(
         model="../../../weights/backbone/llama_7b_hf",
         enable_lora=True,
-        enable_olora=False,
+        enable_olora=enable_olora,
         enforce_eager=True,
         max_loras=5,
         max_lora_rank=16,
@@ -142,5 +213,22 @@ if __name__ == '__main__':
         swap_space=16
     )
     engine = AsyncLLMEngine.from_engine_args(engine_args)
+    
+    # start tracing ...
+    # tracer = trace.Trace(count=False, trace=True)
+    # original_stdout = sys.stdout  # 保存原始的stdout
+    # capturing_file = CapturingFile()
+    # sys.stdout = capturing_file  # 重定向stdout到自定义的文件类
+    
+    # run_trace(tracer=tracer, enable_olora=False)
+    
+    # with open("trace_output2.txt", "w") as f:
+    #     f.write(capturing_file.getvalue())
+
+    # sys.stdout = original_stdout
+    # end tracing ...
+    # profile_program()
+    
+    asyncio.run(nsight_profile_program(engine))
     
     uvicorn.run(app, host="127.0.0.1", port=8001, access_log=False)
